5185b77d2aceb601e60f644ffbf1b1c222d8548a
Ignacio Barria <ibarriao@ripley.com>
2026-03-12 21:50:47 -0300

    add sharded logic and generate blob and merge report on ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,8 +27,8 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 5 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  /* Reporter to use. If CI, use blob reporter. See https://playwright.dev/docs/test-reporters */
+  reporter: process.env.CI ? 'blob' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
